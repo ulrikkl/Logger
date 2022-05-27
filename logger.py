cbe26ed5e6299ed49335834e9e26296e6e541a31
Ulrik Klepsch <klepsch1@live.dk>
2022-05-27 20:15:21 +0200

    done chapter 5

--- logger.py
+++ logger.py
@@ -1,2 +1,6 @@
-import imp
-from app import app
+from app import app, db
+from app.models import User, Role, UserRoles
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Role': Role, 'UserRoles': UserRoles}
